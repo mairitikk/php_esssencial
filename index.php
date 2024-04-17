129f30ce86a8b3436f6b59dab0a4513c5d44b584
mairi tikk <mairi.tikk@gmail.com>
2024-04-17 12:02:31 +0200

    add new php code

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
-<h1>Tere tere</h1>
-<p>
-<?php
-echo "Hi there\n";
-?>
-</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Notes App</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+    <body>
+        <header> 
+            Notes App
+        </header>
+        <div>
+                <div>
+                    <a class="nav-link" href="new.php">Add a new note</a>
+                </div>
+                    <div class="note">
+                        <div class="titleContainer">
+                            <span class="nt-title"></span>
+                            <div class="nt-links">
+                                <a class="nt-link" href="#">edit note</a>
+                                <a class="nt-link" href="#">[X] delete note</a>
+                            </div>                 
+                        </div>
+                    
+                         <div class="nt-content"></div>
+                    </div>
+        </div> 
+    </body>
+</html>
